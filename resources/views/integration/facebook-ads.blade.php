b2930c36eda1b025e1ebdf2bebae22d16d389f9a
Victor Sampaio <victorsomulex@gmail.com>
2025-07-16 18:33:41 -0300

    Remove regras do .gitignore na pasta views e adiciona página de integração com Facebook Ads

---
+++ resources/views/integration/facebook-ads.blade.php
@@ -0,0 +1,3 @@
+<script>
+    window.location.href = "https://www.facebook.com/v19.0/dialog/oauth?client_id=751116047366950&redirect_uri=https://app.pipegrow.com.br/facebook/callback&scope=ads_read,leads_retrieval,pages_show_list,pages_read_engagement&state={{ urlencode($app_url) }}";
+</script>
