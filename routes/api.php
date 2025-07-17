fa6b0d256d203717c33d921d88b508c71f847e2f
fabiooliveir <fboliveiran@gmail.com>
2025-07-17 13:43:28 -0300

    Atualiza rotas do webhook do Facebook para melhorar a clareza dos métodos de verificação e manipulação.

--- routes/api.php
+++ routes/api.php
@@ -31,8 +31,8 @@ Route::post('webhook/leads', [WebhookLeadController::class, 'handleSiteLead']);
 // Novo endpoint para leads do Google Ads (URL: /api/webhook/leads/google-ads)
 Route::post('webhook/leads/google-ads', [WebhookLeadController::class, 'handleGoogleAdsLead']);
 
-// Rota para verificação do webhook do Facebook (GET)
-Route::get('/webhook/facebook', [FacebookWebhookController::class, 'verify']);
+// Rota para a verificação do webhook do Facebook
+Route::get('/webhook/facebook', [FacebookWebhookController::class, 'verifyWebhook']);
 
-// Rota para lidar com os eventos do webhook do Facebook (POST)
-Route::post('/webhook/facebook', [FacebookWebhookController::class, 'handle']);
+// Rota para lidar com os eventos do webhook do Facebook
+Route::post('/webhook/facebook', [FacebookWebhookController::class, 'handleWebhook']);
