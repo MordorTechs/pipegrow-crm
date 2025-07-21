d25d9232ff36df21d8aaf37e753e42c5de04e7d5
fabiooliveir <fboliveiran@gmail.com>
2025-07-20 22:34:09 -0300

    modified:   routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebhookLeadController;
 use App\Http\Controllers\FacebookWebhookController;
+use App\Http\Controllers\WhatsappWebhookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
