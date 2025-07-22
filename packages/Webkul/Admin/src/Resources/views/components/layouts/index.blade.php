5388bebd06f57cc4aeb9d0e9aa09c9a30f91283d
fabiooliveir <fboliveiran@gmail.com>
2025-07-22 19:03:03 -0300

    CRM-10 Facebook SDK for JavaScript

--- packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
@@ -141,6 +141,28 @@
 </head>
 
 <body class="h-full font-inter dark:bg-gray-950">
+
+<!-- Facebook SDK for JavaScript -->
+        <script>
+          window.fbAsyncInit = function() {
+            FB.init({
+              appId      : '751116047366950',
+              xfbml      : true,
+              version    : 'v23.0'
+            });
+            FB.AppEvents.logPageView();
+          };
+
+          (function(d, s, id){
+             var js, fjs = d.getElementsByTagName(s)[0];
+             if (d.getElementById(id)) {return;}
+             js = d.createElement(s); js.id = id;
+             js.src = "https://connect.facebook.net/en_US/sdk.js";
+             fjs.parentNode.insertBefore(js, fjs);
+           }(document, 'script', 'facebook-jssdk'));
+        </script>
+        <!-- Fim do Facebook SDK -->
+
     {!! view_render_event('admin.layout.body.before') !!}
 
     <div
